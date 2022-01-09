e597ead09e694601e754c9fa33f0682b3c72157e
Dan Prince <danpprince@gmail.com>
2022-01-09 18:24:09 -0500

    Add CLI arg to yt download script

--- download_yt_video.py
+++ download_yt_video.py
@@ -1,11 +1,16 @@
 import pytube
+import sys
 
-YT_URL = "https://www.youtube.com/watch?v=IMPbKVb8y8s"
 
 if __name__ == "__main__":
-    yt = pytube.YouTube(YT_URL)
+    args = sys.argv
+    if len(args) < 2:
+        raise ValueError("Expected YouTube URL as argument")
+
+    yt_url = sys.argv[1]
+    yt = pytube.YouTube(yt_url)
 
     # Get the highest resolution mp4 stream
     yt_stream = yt.streams.filter(file_extension="mp4", progressive=True).order_by("resolution").last()
 
-    yt_stream.download()
+    yt_stream.download("videos/")
