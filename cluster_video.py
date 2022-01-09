932efbbfeefe170160a6dedc09ec90c0bbcecdcb
Dan Prince <danpprince@gmail.com>
2022-01-09 11:54:01 -0500

    Add SubFrame dataclass and plot cluster time distribution

--- cluster_video.py
+++ cluster_video.py
@@ -1,7 +1,8 @@
 import argparse
+from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
-from typing import Any, List, Tuple
+from typing import Any, List, Optional, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,12 +18,26 @@ OUTPUT_DIR = Path("output")
 NUM_CLUSTERS = 40
 
 
+@dataclass
+class SubFrame:
+    frame_index: int
+    timestamp: float
+    start_row: int
+    end_row: int
+    start_col: int
+    end_col: int
+    img: np.ndarray
+    features: Optional[np.ndarray] = None
+    standardized_features: Optional[np.ndarray] = None
+    cluster_index: Optional[int] = None
+
+
 def get_clusters_for_video(video: Path) -> Any:
     process_frame_rate = 200
 
-    features = []
     subframes = []
     with imageio.get_reader(video, format="FFMPEG") as reader:
+        video_frame_rate = reader.get_meta_data()["fps"]
         total_frames = reader.count_frames()
         for index in tqdm(
             range(0, total_frames, process_frame_rate), desc="Extracting subframes"
@@ -30,17 +45,20 @@ def get_clusters_for_video(video: Path) -> Any:
             frame = reader.get_data(index)
 
             if index % process_frame_rate == 0:
-                current_subframes = extract_subframes_from_frame(frame, index)
+                current_subframes = extract_subframes_from_frame(
+                    frame, index, video_frame_rate
+                )
                 subframes.extend(current_subframes)
-                current_features = extract_features_from_subframes(current_subframes)
-                features.extend(current_features)
 
-    clusters, standardized_features = cluster_features(features)
+    clusters, standardized_features = cluster_subframes(subframes)
     visualize_clusters(clusters, subframes, standardized_features)
 
 
 def extract_subframes_from_frame(
-    frame: np.ndarray, frame_index: int, write_subframes: bool = False
+    frame: np.ndarray,
+    frame_index: int,
+    frame_rate: float,
+    write_subframes: bool = False,
 ) -> List[np.ndarray]:
     subframe_size_px = 80
 
@@ -53,6 +71,7 @@ def extract_subframes_from_frame(
     subframes = []
     for row_index in range(num_rows):
         for col_index in range(num_cols):
+            timestamp = frame_index / frame_rate
 
             start_row = row_index * subframe_size_px
             end_row = (row_index + 1) * subframe_size_px
@@ -64,35 +83,30 @@ def extract_subframes_from_frame(
                 start_col:end_col,
                 :,
             ]
-            subframes.append(subframe_img)
 
-            # SubFrame(
-            #     frame_index=frame_index,
-            #     start_row=start_row,
-            #     end_row=end_row,
-            #     start_col=start_col,
-            #     end_col=end_col,
-            # )
-
-            if write_subframes:
-                output_path = Path("output") / (
-                    f"frame{frame_index :06d}"
-                    f"_row{row_index :02d}_col{col_index :02d}.png"
-                )
-                imageio.imwrite(output_path, subframe_img)
+            sf = SubFrame(
+                frame_index=frame_index,
+                timestamp=timestamp,
+                start_row=start_row,
+                end_row=end_row,
+                start_col=start_col,
+                end_col=end_col,
+                img=subframe_img,
+            )
+            sf.features = extract_features_from_image(sf.img)
+            subframes.append(sf)
 
     return subframes
 
 
-def extract_features_from_subframes(subframes: List[np.ndarray]) -> List[np.ndarray]:
-    subframe_features = [
-        np.concatenate((extract_color_features(f), extract_edge_features(f)))
-        for f in subframes
-    ]
+def extract_features_from_image(img: np.ndarray) -> np.ndarray:
+    subframe_features = np.concatenate(
+        (extract_color_features(img), extract_edge_features(img))
+    )
     return subframe_features
 
 
-def extract_color_features(img: np.ndarray, bins_per_channel: int = 4) -> np.ndarray:
+def extract_color_features(img: np.ndarray, bins_per_channel: int = 8) -> np.ndarray:
     hist_min_value = np.iinfo(img.dtype).min
     hist_max_value = np.iinfo(img.dtype).max
     bin_edges = np.linspace(hist_min_value, hist_max_value, bins_per_channel + 1)
@@ -110,7 +124,7 @@ def extract_color_features(img: np.ndarray, bins_per_channel: int = 4) -> np.nda
     return np.concatenate(features)
 
 
-def extract_edge_features(img: np.ndarray, num_bins: int = 4) -> np.ndarray:
+def extract_edge_features(img: np.ndarray, num_bins: int = 8) -> np.ndarray:
     hist_min_value = 0.0
     hist_max_value = 0.75
     bin_edges = np.linspace(hist_min_value, hist_max_value, num_bins + 1)
@@ -131,23 +145,48 @@ def extract_edge_features(img: np.ndarray, num_bins: int = 4) -> np.ndarray:
     return edge_hist
 
 
-def cluster_features(features: List[np.ndarray]) -> Tuple[KMeans, np.ndarray]:
+def cluster_subframes(subframes: List[SubFrame]) -> Tuple[KMeans, np.ndarray]:
     scaler = StandardScaler()
     kmeans = KMeans(n_clusters=NUM_CLUSTERS, random_state=0)
 
+    features = np.stack([sf.features for sf in subframes])
+
     standardized_features = scaler.fit_transform(features)
     kmeans.fit(standardized_features)
+
+    for index, sf in enumerate(subframes):
+        sf.standardized_features = standardized_features[index, :]
+        sf.cluster_index = kmeans.labels_[index]
+
     return kmeans, standardized_features
 
 
 def visualize_clusters(
-    clusters: KMeans, subframes: List[np.ndarray], features: np.ndarray
+    clusters: KMeans, subframes: List[SubFrame], features: np.ndarray
 ) -> None:
 
     now_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     now_timestamp_dir = OUTPUT_DIR / now_timestamp
 
-    centroid_subframes = []
+    plt.figure(figsize=(10, 8))
+    for cluster_index in range(clusters.n_clusters):
+        cluster_dir = now_timestamp_dir / f"cluster-{cluster_index :03d}"
+        cluster_dir.mkdir(parents=True, exist_ok=True)
+
+        timestamps = [
+            sf.timestamp for sf in subframes if sf.cluster_index == cluster_index
+        ]
+        plt.hist(timestamps, bins=20)
+
+        ticks = np.arange(0, timestamps[-1], 30)
+        tick_labels = [f"{t // 60 :0.0f}:{t % 60 :02.0f}" for t in ticks]
+        plt.xticks(ticks, tick_labels)
+
+        plt.title(f"Time distribution of cluster {cluster_index}")
+        plt.tight_layout()
+        plt.savefig(cluster_dir / "time-distribution.png")
+
+    centroid_imgs = []
     for cluster_index in range(clusters.n_clusters):
         cluster_dir = now_timestamp_dir / f"cluster-{cluster_index :03d}"
         cluster_dir.mkdir(parents=True, exist_ok=True)
@@ -157,20 +196,20 @@ def visualize_clusters(
 
         distances = np.mean((feature_centroid - features) ** 2, axis=1)
         centroid_subframe_index = np.argmin(distances)
-        centroid_subframe = subframes[centroid_subframe_index]
-        centroid_subframes.append(centroid_subframe)
+        centroid_img = subframes[centroid_subframe_index].img
+        centroid_imgs.append(centroid_img)
 
         centroid_subframe_path = cluster_dir / "centroid_subframe.png"
-        imageio.imwrite(centroid_subframe_path, centroid_subframe)
+        imageio.imwrite(centroid_subframe_path, centroid_img)
 
     plt.figure(figsize=(10, 8))
     num_rows = 5
     num_cols = NUM_CLUSTERS // num_rows
-    for cluster_index, centroid_subframe in enumerate(centroid_subframes):
+    for cluster_index, centroid_img in enumerate(centroid_imgs):
         num_subframes_in_cluster = np.sum(clusters.labels_ == cluster_index)
 
         plt.subplot(num_rows, num_cols, cluster_index + 1)
-        plt.imshow(centroid_subframe)
+        plt.imshow(centroid_img)
         plt.xticks([])
         plt.yticks([])
         plt.title(
@@ -181,7 +220,7 @@ def visualize_clusters(
     plt.savefig(now_timestamp_dir / "clusters.png")
 
     for subframe_index, cluster_index in enumerate(clusters.labels_):
-        subframe_img = subframes[subframe_index]
+        subframe_img = subframes[subframe_index].img
 
         cluster_dir = OUTPUT_DIR / now_timestamp / f"cluster-{cluster_index :03d}"
         imageio.imwrite(
